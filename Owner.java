fbc080989fcbd13dc86e77d0b833de8e511580a6
aanadhila <89927601+aanadhila@users.noreply.github.com>
2021-11-17 13:53:35 +0700

    Add files via upload

---
+++ Owner.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polimorfisme;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Owner {
+    public void pay(Payable p){
+        System.out.println("Total payment = "+p.getPaymentAmount());
+        if (p instanceof ElectricityBill) {
+            ElectricityBill eb = (ElectricityBill) p;
+            System.out.println(""+eb.getBillInfo());
+        } else if(p instanceof PermanentEmployee){
+            PermanentEmployee pe = (PermanentEmployee) p;
+            pe.getEmployeeInfo();
+            System.out.println(""+pe.getEmployeeInfo());
+        }
+    }
+    public void showMyEmployee(Employee e){
+        System.out.println(""+e.getEmployeeInfo());
+        if (e instanceof PermanentEmployee) {
+            System.out.println("You have to pay her/him monthly!!!");
+        } else {
+            System.out.println("No need to pay him/her");
+        }
+    }
+
+    void pay(InternshipEmployee iEmp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
